24309c806c0a217170767c4c824f85e9bf0d0acc
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-11-01 17:58:27 +0100

    Gjør siden barebones - fjern unødvendig ræl

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,11 +10,37 @@ export default function RootLayout({
   children: ReactNode
 }) {
   return (
-    <html lang="no">
+    <html lang="en">
       <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@JavaZone" />
+        <meta name="twitter:image:alt" content="JavaZone logo" />
+        <meta
+          name="description"
+          content="JavaZone - The biggest community-driven Java conference."
+        />
+        <meta property="og:title" content="JavaZone 2023" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://2023.javazone.no" />
+        <meta property="og:description" content="The biggest community-driven Java conference." />
+        <meta property="og:site_name" content="javazone.no" />
         <title>JavaZone 2023</title>
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <footer>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Laget av JavaBin
+          </a>
+        </footer>
+      </body>
     </html>
   )
 }
