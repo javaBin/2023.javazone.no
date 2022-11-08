1efb9fd2978bdbb66f1b73138d0356d5ecde974c
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-11-08 19:05:04 +0100

    Legg på security headers

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ npm run dev
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result :-)
+
+Datoer: 6-7. september 2023
+Workshop: 5. september 2023
--- next.config.js
+++ next.config.js
@@ -1,10 +1,65 @@
+const ContentSecurityPolicy = `
+  default-src 'none';
+  script-src 'self';
+  style-src 'self';
+  font-src 'self';
+  base-uri 'self';
+  form-action 'self'
+  frame-ancestors 'none';
+`
+
+const headers = [
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin',
+  },
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+  },
+]
+
+const productionOnlyHeaders = [""]
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
-    appDir: true
-  }
+    appDir: true,
+  },
+  poweredByHeader: false,
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: headers.filter((header) => {
+          if (productionOnlyHeaders.includes(header.key)) return process.env.NODE_ENV === 'production'
+          return true
+        }),
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
