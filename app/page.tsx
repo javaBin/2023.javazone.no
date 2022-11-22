3dc46291e982ffb5cbd354d27762b69c82a06acd
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-11-22 19:26:27 +0100

    Oppdater forsiden med informasjon om workshop dato og link til fjorårets program

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 import styles from "./page.module.css"
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>JavaZone 2023</h1>
-      <h2>September 6-7, 2023</h2>
-      <h3>Oslo Spektrum, Norway</h3>
+      <h2>Conference: 6-7 september, 2023</h2>
+      <h2>Workshop: 5 september, 2023</h2>
+      <h3>Location: Oslo Spektrum, Norway</h3>
+      <aside>
+        <p>Click <Link href="https://2022.javazone.no/#/program">here</Link> to view the program of JavaZone 2022</p>
+      </aside>
     </main>
   )
 }
