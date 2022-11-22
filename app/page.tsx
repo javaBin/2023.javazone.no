600fb85933e2b27853915ae3bd5156b23c182501
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-11-22 19:31:53 +0100

    Legg til informasjon om partnermøtet

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,11 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h1>JavaZone 2023</h1>
-      <h2>Conference: 6-7 september, 2023</h2>
-      <h2>Workshop: 5 september, 2023</h2>
+      <h2>Conference: September 6-7, 2023</h2>
+      <h2>Workshop: September 5, 2023</h2>
       <h3>Location: Oslo Spektrum, Norway</h3>
-      <aside>
         <p>Click <Link href="https://2022.javazone.no/#/program">here</Link> to view the program of JavaZone 2022</p>
-      </aside>
+        <p className={styles.partnerinfo}>NB! Partner meeting #1 is 6th of December, send us a message at <Link href="mailto:partner@java.no">partner@java.no</Link> if you have not received an invitation.</p>
     </main>
   )
 }
