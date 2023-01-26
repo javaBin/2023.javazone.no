7e72c841344c6abf3b6857fc13937ecdd95a2545
dervism <dervis4@gmail.com>
2023-01-27 00:04:21 +0100

    Updated mobile menu

--- app/header.tsx
+++ app/header.tsx
@@ -1,8 +1,14 @@
-import Link from 'next/link'
+'use client';
 
+import { useState } from 'react';
+
+import Link from 'next/link'
 import Image from "next/image";
 
 export function Header() {
+
+  const [burger, setBurger] = useState(false);
+
   return (
   <nav className="navbar is-dark is-fixed-top" role="navigation" aria-label="main navigation">
     <div className="navbar-brand">
@@ -10,7 +16,10 @@ export function Header() {
         <Image src="/logo-sharp.svg" height={48} width={48}  alt="logo"/>
       </a>
 
-      <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false"
+      <a role="button"
+         onClick={() => setBurger(!burger)}
+         className={burger ? "navbar-burger is-active" : "navbar-burger"}
+         aria-label="menu" aria-expanded="false"
          data-target="navbarBasicExample">
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
@@ -18,7 +27,7 @@ export function Header() {
       </a>
     </div>
 
-    <div id="navbarBasicExample" className="navbar-menu">
+    <div id="navbarBasicExample" className={burger ? "navbar-menu is-active" : "navbar-menu"}>
       <div className="navbar-start">
         <Link className="navbar-item" href="/">JavaZone 2023 - JavaBin</Link>
 
@@ -30,11 +39,9 @@ export function Header() {
       <div className="navbar-end">
         <div className="navbar-item">
           <div className="buttons">
-            <a className="button is-primary">
-              <strong>
-                <Link href="/speakers">Submit your talk!</Link>
-              </strong>
-            </a>
+            <strong>
+              <Link className="button is-primary" href="/speakers">Submit your talk!</Link>
+            </strong>
           </div>
         </div>
       </div>
