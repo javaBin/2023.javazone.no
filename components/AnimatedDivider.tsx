20ce2e5b41986b294431c8779c653aa0cc04f369
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2023-03-25 16:24:18 +0100

    AnimatedDivider component

---
+++ components/AnimatedDivider.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import Image from 'next/image'
+
+const dividers = [1, 2, 3, 4, 5]
+
+export default function AnimatedDivider() {
+  const [currentDivider, setCurrentDivider] = useState(1)
+  const dividerUrl = `/svg/JZ2023-Divider-${currentDivider}.svg`
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentDivider((prevDivider) => {
+        console.log(prevDivider)
+        return prevDivider === dividers.length ? 1 : prevDivider + 1
+      })
+    }, 300)
+    return () => clearInterval(interval)
+  }, [])
+
+  return <Image src={dividerUrl} height={60} width={60} alt="divider" />
+}
