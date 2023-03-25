f7ce8223151194c8164d6f29572d5a364db203dc
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2023-03-25 13:19:06 +0100

    Få inn metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,32 @@ import '../styles/globals.scss'
 import { Footer } from './footer'
 import { Header } from './header'
 import Head from "next/head";
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+  themeColor: '#fff',
+  icons: "/images/javazone2023-fav.png",
+  twitter: {
+    card: 'summary',
+    title: "JavaZone",
+    site: '@javazone',
+    creator: 'JavaZone',
+    images: "https://pbs.twimg.com/profile_images/1635386828239912960/Wrrd1NMU_400x400.png",
+    description: 'JavaZone - The biggest community-driven Java conference.',
+  },
+  openGraph: {
+    title: 'JavaZone 2023',
+    type: 'website',
+    url: 'https://2023.javazone.no',
+    description: 'The biggest community-driven Java conference.',
+    siteName: 'javazone.no',
+  },
+}
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <Head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no" />
         <meta name="theme-color" content="#fff" />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@JavaZone" />
