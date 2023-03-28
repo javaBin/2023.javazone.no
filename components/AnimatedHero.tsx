3b612007f6a2b2914c4582c8cb58cf14ac6f5644
Ellaoui94 <abab010@student.kristiania.no>
2023-03-29 01:27:31 +0200

    Small change

--- components/AnimatedHero.tsx
+++ components/AnimatedHero.tsx
@@ -5,6 +5,6 @@ import Lottie from 'lottie-react';
 import animationData from '../public/animations/JavaZone-hero-flags.json';
 import styles from './animatedHero.module.css';
 
-const AnimatedHero = () => <Lottie animationData={animationData} loop={true} className={styles.animatedHero}/>;
+const AnimatedHero = () => <Lottie animationData={animationData} loop={false} className={styles.animatedHero}/>;
 
 export default AnimatedHero;
