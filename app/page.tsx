231a18960cd65e262b2f4def40e7cbd89a877483
Ellaoui94 <abab010@student.kristiania.no>
2023-04-13 11:47:52 +0200

    Added Wendy and Wheely to main page with animations

--- app/page.tsx
+++ app/page.tsx
@@ -3,51 +3,64 @@ import styles from './page.module.css'
 import AnimatedDivider from "../components/AnimatedDivider";
 import AnimatedHero from "../components/AnimatedHero";
 import AnimatedFireworks from "../components/AnimatedFireworks";
+import Image from "next/image";
 
 export default function Home() {
 
 
   return (
-    <section className={`has-text-centered`}>
+    <div className={"columns"}>
 
-      <div className="mb-4">
-        <AnimatedHero/>
+      <div className={styles.wendy}>
+        <Image src={"/svg/JZ2023-Character-Wendy-cropped.svg"} width={300} height={300} alt={"f"}/>
       </div>
 
-      <div>
-        <AnimatedDivider/>
-         <AnimatedFireworks/>
-      </div>
+      <section className={`has-text-centered`}>
 
-      <strong>
-        <Link className="button is-dark-blue mt-4" href="/speakers">
-          Submit your talk!
-        </Link>
-      </strong>
+        <div className="mb-4">
+          <AnimatedHero/>
+        </div>
 
-      <h1 className={`title ${styles.fontAndColor} mt-4`}>
-        Welcome to the The Amazing JavaZone 6-7 September in Oslo Spektrum
-      </h1>
-      <div className={'mb-4'}>
-        <p>Conference: September 6-7, 2023</p>
-        <p>Workshop: September 5, 2023</p>
-        <p>Location: Oslo Spektrum, Norway</p>
-      </div>
-      <div>
         <div>
-          Click <Link href="https://2022.javazone.no/#/program">here</Link> to view the program of
-          JavaZone 2022
+          <AnimatedDivider/>
+          <AnimatedFireworks/>
+        </div>
+
+        <strong>
+          <Link className="button is-dark-blue mt-4" href="/speakers">
+            Submit your talk!
+          </Link>
+        </strong>
+
+        <h1 className={`title ${styles.fontAndColor} mt-4`}>
+          Welcome to the The Amazing JavaZone 6-7 September in Oslo Spektrum
+        </h1>
+        <div className={'mb-4'}>
+          <p>Conference: September 6-7, 2023</p>
+          <p>Workshop: September 5, 2023</p>
+          <p>Location: Oslo Spektrum, Norway</p>
         </div>
         <div>
-          <Link href="/tickets">Ticket sale</Link> opens February 20th
+          <div>
+            Click <Link href="https://2022.javazone.no/#/program">here</Link> to view the program of
+            JavaZone 2022
+          </div>
+          <div>
+            <Link href="/tickets">Ticket sale</Link> opens February 20th
+          </div>
+          <div className="has-text-weight-bold">
+            NB! Want to be a partner? Click <Link href="/partners">here</Link>
+          </div>
         </div>
-        <div className="has-text-weight-bold">
-          NB! Want to be a partner? Click <Link href="/partners">here</Link>
+        <div className="mt-6">
+          <AnimatedDivider/>
         </div>
+      </section>
+
+      <div className={styles.wheely}>
+        <Image src={"/svg/JZ2023-Character-Wheely-cropped.svg"} width={400} height={400} alt={"f"}/>
       </div>
-      <div className="mt-6">
-        <AnimatedDivider/>
-      </div>
-    </section>
+
+    </div>
   )
 }
