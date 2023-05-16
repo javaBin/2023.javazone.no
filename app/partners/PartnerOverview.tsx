2bb170602e8346ddc72af72e7c6b8e32ead9ce8f
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2023-05-16 19:08:21 +0200

    Fix style from tailwind

--- app/partners/PartnerOverview.tsx
+++ app/partners/PartnerOverview.tsx
@@ -8,7 +8,7 @@ import '../../styles/globals.css'
  * */
 export default function PartnerOverview() {
   return (
-    <section className="block w-screen bg-[#FED12D] p-4 ml-[calc(-50vw+50%)] mr-[ml-[calc(-50vw+50%)]]">
+    <section className="block w-screen bg-[#FED12D] p-4 ml-[calc(-50vw+50%)] mr-[calc(-50vw+50%)]">
       <h1 className="text-2xl text-center text-black uppercase my-4">Partners in 2023</h1>
       <div className="flex flex-wrap justify-center gap-1.5">
         {[...partners]
