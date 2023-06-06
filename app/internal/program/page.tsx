2f7c9e3d104ff9e2d5fa95accfdcfb05b51a0afb
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2023-06-06 20:23:15 +0200

    Add a preliminary list of previous year talks and workshops

--- app/internal/program/page.tsx
+++ app/internal/program/page.tsx
@@ -1,3 +1,53 @@
-export default function Program() {
-  return <div>Program 2023</div>
+import { Program, SessionFormat, SessionLanguage } from './program'
+
+const formatter = new Intl.DateTimeFormat(['en'], {
+  day: '2-digit',
+  weekday: 'long',
+  hour: 'numeric',
+  minute: 'numeric',
+})
+
+const toFormat = (format: SessionFormat) => {
+  switch (format) {
+    case 'presentation':
+      return 'Presentation'
+    case 'lightning-talk':
+      return 'Lightning talk'
+    case 'workshop':
+      return 'Workshop'
+  }
+}
+
+const toLanguage = (language: SessionLanguage) => (language === 'no' ? 'Norwegian' : 'English')
+
+export default async function ProgramPage() {
+  const data: Program = await fetch(
+    'https://sleepingpill.javazone.no/public/allSessions/javazone_2022',
+  ).then((res) => res.json())
+
+  return (
+    <div>
+      <ul>
+        {data.sessions
+          .sort(
+            (a, b) =>
+              a.startTime.localeCompare(b.startTime) || a.room?.localeCompare(b.room ?? '') || 0,
+          )
+          .map((session) => (
+            <li
+              key={session.id}
+              style={{ backgroundColor: 'black', margin: '2rem', padding: '2rem' }}
+            >
+              <p>{session.title}</p>
+              <p>Speakers: {session.speakers.map((speaker) => speaker.name).join(', ')}</p>
+              <p>{toFormat(session.format)}</p>
+              <p>{toLanguage(session.language)}</p>
+              <p>{formatter.format(new Date(session.startTime))}</p>
+              <p>{formatter.format(new Date(session.endTime))}</p>
+              <p>{session.room}</p>
+            </li>
+          ))}
+      </ul>
+    </div>
+  )
 }
