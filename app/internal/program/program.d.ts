d06aa63e1f4e48cc5be72b58762146220f45af57
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2023-06-06 17:21:32 +0200

    Start with basic files

---
+++ app/internal/program/program.d.ts
@@ -0,0 +1,35 @@
+export interface Program {
+  sessions: Session[]
+}
+
+export type SessionLanguage = "no" | "en"
+
+export type SessionFormat = "presentation" | "lightning-talk" | "workshop"
+
+export interface Session {
+  intendedAudience: string
+  length: string
+  format: SessionFormat
+  language: SessionLanguage
+  abstract: string
+  title: string
+  room?: string
+  startTime: string
+  endTime: string
+  video?: string
+  startTimeZulu: string
+  endTimeZulu: string
+  id: string
+  sessionId: string
+  conferenceId: string
+  startSlot: string
+  startSlotZulu: string
+  speakers: Speaker[]
+  registerLoc?: string
+}
+
+export interface Speaker {
+  name: string
+  bio?: string
+  twitter: string
+}
