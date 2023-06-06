9d4e2a77da733febc05919a99c3d1111e869df2b
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2023-06-06 21:27:29 +0200

    Add radio buttons for filters

--- app/internal/program/page.tsx
+++ app/internal/program/page.tsx
@@ -1,4 +1,5 @@
 import { Program, SessionFormat, SessionLanguage } from './program'
+import { Fragment } from 'react'
 
 const formatter = new Intl.DateTimeFormat(['en'], {
   day: '2-digit',
@@ -7,7 +8,7 @@ const formatter = new Intl.DateTimeFormat(['en'], {
   minute: 'numeric',
 })
 
-const prettyFormat = (format: SessionFormat) => {
+const prettyFormat = (format: Filter['format']) => {
   switch (format) {
     case 'presentation':
       return 'Presentation'
@@ -15,23 +16,174 @@ const prettyFormat = (format: SessionFormat) => {
       return 'Lightning talk'
     case 'workshop':
       return 'Workshop'
+    case 'favorites':
+      return 'My favorites'
+    default:
+      return ''
   }
+}
+
+interface Filter {
+  format?: SessionFormat | 'favorites'
+  language?: SessionLanguage
+  room?: string
+  day?: 'tuesday' | 'wednesday' | 'thursday'
+}
+
+const FilterComp = ({ day, room, language = 'no', format = 'presentation' }: Filter) => {
+  console.log({ day, room, language, format })
+  return (
+        <div>
+      <form>
+        <fieldset>
+          <legend>Day</legend>
+          <input
+            type="radio"
+            id="day-all"
+            name="day"
+            value="all"
+            defaultChecked={day === undefined}
+          />
+          <label htmlFor="day-all">All</label>
+          <input
+            type="radio"
+            id="day-tuesday"
+            name="day"
+            value="tuesday"
+            defaultChecked={day === 'tuesday'}
+          />
+          <label htmlFor="day-tuesday">Tuesday</label>
+          <input
+            type="radio"
+            id="day-wednesday"
+            name="day"
+            value="wednesday"
+            defaultChecked={day === 'wednesday'}
+          />
+          <label htmlFor="day-wednesday">Wednesday</label>
+          <input
+            type="radio"
+            id="day-thursday"
+            name="day"
+            value="thursday"
+            defaultChecked={day === 'thursday'}
+          />
+          <label htmlFor="day-thursday">Thursday</label>
+        </fieldset>
+        <fieldset>
+          <legend>Language</legend>
+          <input
+            type="radio"
+            id="language-all"
+            name="language"
+            value="all"
+            defaultChecked={language === undefined}
+          />
+          <label htmlFor="language-all">All</label>
+          <input
+            type="radio"
+            id="language-no"
+            name="language"
+            value="no"
+            defaultChecked={language === 'no'}
+          />
+          <label htmlFor="language-no">Norwegian</label>
+          <input
+            type="radio"
+            id="language-en"
+            name="language"
+            value="en"
+            defaultChecked={language === 'en'}
+          />
+          <label htmlFor="language-en">English</label>
+        </fieldset>
+        <fieldset>
+          <legend>Format</legend>
+          <input
+            type="radio"
+            id="format-all"
+            name="format"
+            value="all"
+            defaultChecked={format === undefined}
+          />
+          <label htmlFor="format-all">All</label>
+          <input
+            type="radio"
+            id="format-presentation"
+            name="format"
+            value="no"
+            defaultChecked={format === 'presentation'}
+          />
+          <label htmlFor="format-presentation">Presentation</label>
+          <input
+            type="radio"
+            id="format-workshop"
+            name="format"
+            value="en"
+            defaultChecked={format === 'workshop'}
+          />
+          <label htmlFor="format-workshop">Workshop</label>
+          <input
+            type="radio"
+            id="format-lightning-talk"
+            name="format"
+            value="en"
+            defaultChecked={format === 'lightning-talk'}
+          />
+          <label htmlFor="format-lightning-talk">Lightning talk</label>
+          <input
+            type="radio"
+            id="format-favorites"
+            name="format"
+            value="en"
+            defaultChecked={format === 'favorites'}
+          />
+          <label htmlFor="format-favorites">My favorites</label>
+        </fieldset>
+        <fieldset>
+          <legend>Room</legend>
+          <input
+            type="radio"
+            id="room-all"
+            name="room"
+            value="all"
+            defaultChecked={room === undefined}
+          />
+          <label htmlFor="room-all">All</label>
+          {[1, 2, 3, 4, 5, 6, 7].map((currentRoom) => (
+            <Fragment key={currentRoom}>
+              <input
+                type="radio"
+                id={`room-${currentRoom}`}
+                name="room"
+                value="all"
+                defaultChecked={currentRoom.toString() === room}
+              />
+              <label htmlFor={`room-${currentRoom}`}>{currentRoom}</label>
+            </Fragment>
+          ))}
+        </fieldset>
+      </form>
+    </div>
+  )
 }
 
 const prettyLanguage = (language: SessionLanguage) => (language === 'no' ? 'Norwegian' : 'English')
 
 export default async function ProgramPage() {
   const data: Program = await fetch(
-    'https://sleepingpill.javazone.no/public/allSessions/javazone_2022', {
+    'https://sleepingpill.javazone.no/public/allSessions/javazone_2022',
+    {
       next: {
         revalidate: 60 * 10, // 10 minute cache
-        tags: ["talks"]
-      }
-    }
+        tags: ['talks'],
+      },
+    },
   ).then((res) => res.json())
 
   return (
     <div>
+      <FilterComp />
       <ul>
         {data.sessions
           .sort(
