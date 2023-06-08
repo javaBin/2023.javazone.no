1faa3072cde8fe278de451932160bb1558628174
Anders Karlsen <karlsenanders@gmail.com>
2023-06-08 21:00:46 +0200

    Updated bullettpoint in superoverflow

--- app/superoverflow/page.tsx
+++ app/superoverflow/page.tsx
@@ -27,7 +27,7 @@ export default function SuperOverflowPage() {
         <h2>What to expect</h2>
         <ul>
           <li>Two conference days, Sept 6th - 7th packed with knowledge, socializing, food and the amazing AweZone Party.</li>
-          <li>The live track will be streamed to Oslo Spektrum.</li>
+          <li>A full live track at Teknologihuset/Rebel with local and international speakers. The Teknologihuset/Rebel live track will also be streamed live to Oslo Spektrum</li>
           <li>Designated conference rooms at Teknologihuset, with live streams from all the sessions in Oslo Spektrum.</li>
           <li>You can post questions to the speakers and evaluate the sessions online.</li>
           <li>A mini overflow from four of the Spektrum tracks.</li>
@@ -67,7 +67,7 @@ export default function SuperOverflowPage() {
         <h2>Conditions</h2>
         <p>All tickets are non-refundable after July 1st 2023, meaning that by purchasing tickets you accept that you cannot under any circumstances demand a refund for such tickets after this date. Notwithstanding the above, you will not receive a refund or be entitled to any other kind of compensation if the event, partly or as a whole, is canceled or postponed due to circumstances outside of our control, including war, strikes, natural disasters, lockdown, power failure, cyber-attacks or any other event that has to be classified as force majeure, as well as any consequences of pandemics including measures laid down by public governmental bodies or corporate policies to mitigate consequences of or health risk related to pandemics at any time.</p>
         <p>In order to make SuperOverflow a success, we need a certain number of participants. We offer tickets from June 12th exclusively to participants who have registered for the ticket waiting list.</p>
-        <p>We will refund the full ticket price if SuperOverflow is canceled because of lack of interest.</p>
+          <p>We will refund the full ticket price if SuperOverflow is canceled because of lack of interest.</p>
       </section>
 
       <section className="content is-normal">
