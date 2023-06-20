f195fc28a890ab95fa2cd83356ec4fc4a592f93a
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2023-06-20 20:28:36 +0200

    Internal program page (#78)
    * Add a preliminary list of previous year talks and workshops
    
    * Rename functions
    
    * Cache response from sleeping pill for 10 minutes
    
    * Tag for future support for ISR revalidation
    
    * Add radio buttons for filters
    
    * Finish style on program page
    
    * Add a readmore component and fix relative imports by @
    
    * Align the readmore to the start
    
    * Add use client to readmore

---
+++ app/internal/program/SessionGroup.tsx
@@ -0,0 +1,19 @@
+import { Session, SessionFormat } from './program'
+import styles from './page.module.css'
+import { prettyFormat } from './utils'
+import { Talk } from './Talk'
+
+export const SessionGroup = ({
+  group,
+  sessions,
+}: {
+  group: SessionFormat
+  sessions: Session[]
+}) => (
+  <ul className={styles.content}>
+    <h2 className={styles.session_title}>{prettyFormat(group)}</h2>
+    {sessions.map((session) => (
+      <Talk session={session} key={session.id} />
+    ))}
+  </ul>
+)
---
+++ components/ReadMore.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import { ReactNode, useState } from 'react'
+
+export const ReadMore = ({ children }: { children: ReactNode }) => {
+  const [open, setIsOpen] = useState(false)
+  return (
+    <>
+      <button
+        className="button is-dark-blue"
+        aria-expanded={open}
+        onClick={() => setIsOpen((current) => !current)}
+      >
+        Read more
+      </button>
+      <div aria-hidden={!open} style={{ display: open ? 'block' : 'none' }}>
+        {children}
+      </div>
+    </>
+  )
+}
