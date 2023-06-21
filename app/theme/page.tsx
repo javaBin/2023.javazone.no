39fbcea12cdff01bcbe1bcc493cf69a583b7be91
Ellaoui94 <abab010@student.kristiania.no>
2023-06-21 20:11:08 +0200

    new fix

--- app/theme/page.tsx
+++ app/theme/page.tsx
@@ -24,18 +24,18 @@ export default function Theme() {
       <h1 className={"title has-text-centered"} style={{marginBottom: "4rem"}}>Why Circus, you might ask?</h1>
 
       <div>
-        <SectionContent src={"/images/JavaZone2022Donut.jpg"} alt={"donut"}
+        <SectionContent src={"/images/JavaZone2022Donut.jpeg"} alt={"donut"}
                         content={"There are several reasons why we chose the circus theme for JavaZone 2023"}
                         contentTwo={
                           "First, the circus is all about having a good time with great people, and that’s what JavaZone is all about too!"}
                         contentThree={"Just like the circus, we want to showcase the amazing skills and talents of our speakers, partners and attendees, and provide an environment where everyone can learn, grow, and be inspired."}/>
 
-        <SectionContent src={"/images/JavaZone2022Rubix.jpg"} alt={"rubix"}
+        <SectionContent src={"/images/JavaZone2022Rubix.jpeg"} alt={"rubix"}
                         content={" Second, we wanted to think outside the box"}
                         contentTwo={"While at the same time being inspired by the important work voluntary entertainers do to make children smile in hospitals and refugee camps."}
                         contentThree={"In addition to the excitement and innovation that a circus brings, we also believe that it aligns perfectly with JavaZone’s commitment to diversity and inclusivity. "}/>
 
-        <SectionContent src={"/images/Javazone2022Arcade.jpg"} alt={"arcade"}
+        <SectionContent src={"/images/Javazone2022Arcade.jpeg"} alt={"arcade"}
                         content={"In addition to the excitement and innovation that a circus brings"}
                         contentTwo={"We also believe that it aligns perfectly\n" +
                           "        with JavaZone’s commitment to diversity and inclusivity. The circus has a long history of celebrating\n" +
@@ -46,7 +46,7 @@ export default function Theme() {
       </div>
 
       <div className={"mt-5"}>
-        <SectionContent src={"/images/JavaZone2022Pool.jpg"} alt={"pool"} content={"Finally"}
+        <SectionContent src={"/images/JavaZone2022Pool.jpeg"} alt={"pool"} content={"Finally"}
                         contentTwo={"We believe that the circus theme will provide a fun and engaging atmosphere for our attendees.\n" +
                           "        Whether it’s the colourful decorations, the exiting tech content, or the legendary AweZone party, we want to\n" +
                           "        create an environment that encourages attendees to let loose, have fun, and make lasting connections with\n" +
@@ -66,7 +66,7 @@ const SectionContent = (props: {
 }) => {
   return (
     <section className={`content is-normal ${styles.displayFlex}`} style={props.style}>
-      <Image src={props.src} data-aos="fade-right" data-aos-duration="1000" width={350} height={200} alt={props.alt}/>
+      <Image src={props.src} data-aos="fade-right" data-aos-duration="1000" width={400} height={200} alt={props.alt}/>
       <div className={voulenterstyles.pDiv} data-aos="fade" data-aos-duration="1000" data-aos-delay="500">
         <h1>{props.content}</h1>
         {props.contentTwo ? <p>{props.contentTwo}</p> : <></>}
