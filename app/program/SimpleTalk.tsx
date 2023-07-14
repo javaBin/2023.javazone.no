0e353dcba047097fac65cbd3cfa3731f762b4576
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2023-07-14 13:53:10 +0200

    Update styling of simpletalk

--- app/program/SimpleTalk.tsx
+++ app/program/SimpleTalk.tsx
@@ -8,15 +8,17 @@ export const SimpleTalk = ({
 }: {
   session: Session
 }) => (
-  <li className={styles.session}>
-    <h3 className={styles.session_title}>{title}</h3>
-    <p>Speakers: {speakers.map((speaker) => speaker.name).join(', ')}</p>
-    <p>Type: {prettyFormat(format)}</p>
-    <p>Language: {prettyLanguage(language)}</p>
-    <p>Length: {length} minutes</p>
-    {startTime && <p>{formatter.format(new Date(startTime))}</p>}
-    {endTime && <p>{formatter.format(new Date(endTime))}</p>}
-    {room && <p>{room}</p>}
-    <Link href={`/program/${id}`}>Read more</Link>
-  </li>
+  <Link href={`/program/${id}`} className={styles.session_link}>
+    <li className={styles.session}>
+      <h3 className={styles.session_title}>{title}</h3>
+      <p>Speakers: {speakers.map((speaker) => speaker.name).join(', ')}</p>
+      <p>Type: {prettyFormat(format)}</p>
+      <p>Language: {prettyLanguage(language)}</p>
+      <p>Length: {length} minutes</p>
+      {startTime && <p>{formatter.format(new Date(startTime))}</p>}
+      {endTime && <p>{formatter.format(new Date(endTime))}</p>}
+      {room && <p>{room}</p>}
+    </li>
+  </Link>
 )
+
