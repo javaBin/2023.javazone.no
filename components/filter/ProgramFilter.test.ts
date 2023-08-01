5049959b11ea9d323f4288d0848802d8143df0a6
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2023-08-01 13:11:29 +0200

    Add filter with tests

---
+++ components/filter/ProgramFilter.test.ts
@@ -0,0 +1,104 @@
+// Opprett noen dummy data for testing
+import { Session, SessionFormat, SessionLanguage } from '@/app/program/program'
+import { filter } from './ProgramFilter'
+
+const dummySession1: Session = {
+  intendedAudience: 'Developers',
+  length: '30 mins',
+  format: 'presentation',
+  language: 'en',
+  abstract: 'This is a dummy session',
+  title: 'Dummy Session 1',
+  startTime: '2023-08-15T10:00:00',
+  endTime: '2023-08-15T10:30:00',
+  startTimeZulu: '2023-08-15T08:00:00Z',
+  endTimeZulu: '2023-08-15T08:30:00Z',
+  id: 'session1',
+  sessionId: 'session1',
+  conferenceId: 'conf1',
+  startSlot: '10',
+  startSlotZulu: '08',
+  speakers: [{ name: 'John Doe', twitter: '@johndoe' }],
+}
+
+const dummySession2: Session = {
+  intendedAudience: 'Designers',
+  length: '1 hour',
+  format: 'lightning-talk',
+  language: 'no',
+  abstract: 'This is another dummy session',
+  title: 'Dummy Session 2',
+  startTime: '2023-08-15T14:00:00',
+  endTime: '2023-08-15T15:00:00',
+  startTimeZulu: '2023-08-15T12:00:00Z',
+  endTimeZulu: '2023-08-15T13:00:00Z',
+  id: 'session2',
+  sessionId: 'session2',
+  conferenceId: 'conf1',
+  startSlot: '14',
+  startSlotZulu: '12',
+  speakers: [{ name: 'Jane Doe', twitter: '@janedoe' }],
+}
+
+const dummyProgram = [dummySession1, dummySession2]
+
+describe('filter', () => {
+  it('should filter sessions by date', () => {
+    const filterOptions = {
+      date: '2023-08-15',
+    }
+    const filteredSessions = filter(dummyProgram, filterOptions)
+    expect(filteredSessions).toEqual([dummySession1, dummySession2])
+  })
+
+  it('should filter sessions by sessionFormat', () => {
+    const filteredSessions = filter(dummyProgram, {
+      format: 'lightning-talk',
+    })
+    expect(filteredSessions).toEqual([dummySession2])
+  })
+
+  it('should filter sessions by language', () => {
+    const filterOptions = {
+      language: 'en' as SessionLanguage,
+    }
+    const filteredSessions = filter(dummyProgram, filterOptions)
+    expect(filteredSessions).toEqual([dummySession1])
+  })
+
+  it('should filter sessions by favorites', () => {
+    const filterOptions = {
+      favorites: ['session1'],
+    }
+    const filteredSessions = filter(dummyProgram, filterOptions)
+    expect(filteredSessions).toEqual([dummySession1])
+  })
+
+  it('should filter sessions by multiple criteria', () => {
+    const filterOptions = {
+      date: '2023-08-15',
+      sessionFormat: 'presentation' as SessionFormat,
+      language: 'en' as SessionLanguage,
+      favorites: ['session1'],
+    }
+    const filteredSessions = filter(dummyProgram, filterOptions)
+    expect(filteredSessions).toEqual([dummySession1])
+  })
+
+  it('should return all sessions if no filters are specified', () => {
+    const filterOptions = {}
+    const filteredSessions = filter(dummyProgram, filterOptions)
+    expect(filteredSessions).toEqual([dummySession1, dummySession2])
+  })
+
+  it('should return an empty array if no sessions match the filters', () => {
+    const filterOptions = {
+      date: '2023-08-16',
+      sessionFormat: 'workshop' as SessionFormat,
+      language: 'en' as SessionLanguage,
+      favorites: ['session1'],
+    }
+    const filteredSessions = filter(dummyProgram, filterOptions)
+    expect(filteredSessions).toEqual([])
+  })
+})
