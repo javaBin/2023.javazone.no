e4e3c25add5eb8335cf5ac55e21bb21862c77b52
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2023-08-25 12:11:42 +0200

    Remove console log

--- app/(app-partners)/app-partners/page.tsx
+++ app/(app-partners)/app-partners/page.tsx
@@ -1,11 +1,11 @@
 import dynamic from 'next/dynamic'
 import { partners } from '@/app/(main)/api/partners/partners'
 import React from 'react'
-import * as console from "console";
 
-const PartnerOverview = dynamic(() => import('@/app/(main)/partners/PartnerOverview'), { ssr: false })
+const PartnerOverview = dynamic(() => import('@/app/(main)/partners/PartnerOverview'), {
+  ssr: false,
+})
 
 export default function Page() {
-  console.log("hello")
   return <PartnerOverview partners={partners} />
 }
