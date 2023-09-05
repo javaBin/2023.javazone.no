315659b112240a3fa0c9ad052dc7afd82e1b0ff7
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2023-09-05 21:19:22 +0200

    Add information about opening and show

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -25,15 +25,21 @@ export default function Home() {
         <Title type="big-title">
           Welcome to The Amazing JavaZone 6-7 September in Oslo Spektrum
         </Title>
-        <Title type="subtitle">Join the Discord: <Link href={"https://discord.javazone.no"}>discord.javazone.no</Link> 🔥</Title>
+        <Title type="subtitle">
+          Join the Discord: <Link href={'https://discord.javazone.no'}>discord.javazone.no</Link> 🔥
+        </Title>
         <div className={'mb-4'}>
+          <p className="mb-4">
+            Our doors open at <strong>8:00</strong> AM on Wednesday September 6th. At <strong>8:30</strong> AM, you can expect our unique
+            opening show taking place in the expo area. The talks will start at <strong>9:00</strong> AM both days
+          </p>
           <p>Conference: September 6-7, 2023</p>
           <p>Workshop: September 5, 2023</p>
           <p>Location: Oslo Spektrum, Norway</p>
         </div>
         <div>
           <div>
-            <Link href="/program">Program  is now ready! 🚀</Link>
+            <Link href="/program">Program is now ready! 🚀</Link>
           </div>
           <div>
             <Link href="/info">General information can be found here</Link>
