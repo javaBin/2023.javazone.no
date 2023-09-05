ecb9b85233797fa4b463b46698e60cc41982e64a
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2023-09-05 17:03:58 +0200

    Add emoji

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
         <Title type="big-title">
           Welcome to The Amazing JavaZone 6-7 September in Oslo Spektrum
         </Title>
-        <Title type="subtitle">Join the Discord: <Link href={"https://discord.javazone.no"}>discord.javazone.no</Link></Title>
+        <Title type="subtitle">Join the Discord: <Link href={"https://discord.javazone.no"}>discord.javazone.no</Link> 🔥</Title>
         <div className={'mb-4'}>
           <p>Conference: September 6-7, 2023</p>
           <p>Workshop: September 5, 2023</p>
